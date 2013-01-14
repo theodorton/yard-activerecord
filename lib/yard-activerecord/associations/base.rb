a927b917c402d5c463379bf648ef7023be2393fd
Petr Chalupa (Pitr) <git@pitr.ch>
2013-01-14 13:36:17 +0100

    white-space formatting

--- lib/yard-activerecord/associations/base.rb
+++ lib/yard-activerecord/associations/base.rb
@@ -4,28 +4,29 @@ require 'active_support/inflector'
 module YARD::Handlers::Ruby::ActiveRecord::Associations
   class Base < YARD::Handlers::Ruby::MethodHandler
     namespace_only
-  
+
     def process
-      object = YARD::CodeObjects::MethodObject.new(namespace, method_name)
-      object.group = "Active Record Associations"
+      group_name = "Active Record Associations"
+      namespace.groups << group_name unless namespace.groups.include? group_name
+
+      object           = YARD::CodeObjects::MethodObject.new(namespace, method_name)
+      object.group     = group_name
       object.docstring = return_description
       object.docstring.add_tag get_tag(:return, '', class_name)
       object.docstring.add_tag get_tag(:see, 'http://api.rubyonrails.org/classes/ActiveRecord/Associations/ClassMethods.html')
       object.dynamic = true
       register object
-      group_name = "Active Record Associations"
-      namespace.groups << group_name unless namespace.groups.include? group_name
     end
-  
+
     private
     def method_name
       call_params[0]
     end
-  
+
     def class_name(singularize = false)
       param_size = statement.parameters.size
       if param_size > 2
-        i = 1
+        i           = 1
         return_this = false
         while i < param_size - 1
           # May want to evaluate doing it this way
@@ -40,11 +41,11 @@ module YARD::Handlers::Ruby::ActiveRecord::Associations
         method_name.capitalize
       end
     end
-  
+
     def return_description
       "An array of associated #{method_name}."
     end
-  
+
     def get_tag(tag, text, return_classes = [])
       YARD::Tags::Tag.new(tag, text, [return_classes].flatten)
     end
