74e15cb25e5bbf12ecd299bb9cfdc9d02ea0a1d4
Nathan Stitt <nathan@stitt.org>
2014-02-18 21:03:36 -0600

    Set return tag on field if documentation lacks it

--- lib/yard-activerecord/fields/field_handler.rb
+++ lib/yard-activerecord/fields/field_handler.rb
@@ -8,21 +8,25 @@ module YARD::Handlers::Ruby::ActiveRecord::Fields
     handles method_call(:timestamp)
     handles method_call(:datetime)
     handles method_call(:date)
-  
+
     def process
       return unless statement.namespace.jump(:ident).source == 't'
       method_name = call_params.first
-      
+
       return if method_name['_id'] # Skip all id fields, associations will handle that
-      
+
       ensure_loaded! P(globals.klass)
       namespace = P(globals.klass)
       return if namespace.nil?
 
       method_definition = namespace.instance_attributes[method_name.to_sym] || {}
-      
+
       { read: method_name, write: "#{method_name}=" }.each do |(rw, name)|
-        next if method_definition[rw]
+        method = method_definition[rw]
+        if method
+          method.docstring.add_tag( get_tag(:return, '', class_name) ) unless method.has_tag?( :return )
+          next
+        end
         rw_object = YARD::CodeObjects::MethodObject.new(namespace, name)
         rw_object.docstring = description(name)
         rw_object.docstring.add_tag get_tag(:return, '', class_name)
@@ -33,11 +37,11 @@ module YARD::Handlers::Ruby::ActiveRecord::Fields
 
       namespace.instance_attributes[method_name.to_sym] = method_definition
     end
-  
+
     def description(method_name)
       '' # "Database field value of #{method_name}. Defined in {file:db/schema.rb}"
     end
-  
+
     def get_tag(tag, text, return_classes)
       YARD::Tags::Tag.new(:return, text, [return_classes].flatten)
     end
